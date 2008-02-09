5d24faf68ee2fa532a7e21d4fcb9e9b77c4de537
russo2503v <russo2503v@4b7f92db-2845-0410-95f1-b1f669fef64b>
2008-02-09 05:23:40 +0000

    including submitted directory when enabled
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@54 4b7f92db-2845-0410-95f1-b1f669fef64b

--- cmake/UseBasicRules.cmake
+++ cmake/UseBasicRules.cmake
@@ -5,6 +5,14 @@
 INCLUDE_DIRECTORIES( ${GBX_PROJECT_SOURCE_DIR}/src )
 
 #
+# Do the same for the submitted directory when its compilation
+# is enabled.
+#
+IF ( GBX_BUILD_SUBMITTED )
+    INCLUDE_DIRECTORIES( ${GBX_PROJECT_SOURCE_DIR}/submitted )
+ENDIF ( GBX_BUILD_SUBMITTED )
+
+#
 # Platform-specific compiler and linker flags
 #
 IF ( NOT OS_WIN )
