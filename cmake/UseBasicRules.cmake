deae72f80d35ae6e9ec0d87f81a20ce4c36eea99
russo2503v <russo2503v@4b7f92db-2845-0410-95f1-b1f669fef64b>
2008-02-10 04:43:57 +0000

    corrected names of OS variables
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@57 4b7f92db-2845-0410-95f1-b1f669fef64b

--- cmake/UseBasicRules.cmake
+++ cmake/UseBasicRules.cmake
@@ -15,8 +15,8 @@ ENDIF ( GBX_BUILD_SUBMITTED )
 #
 # Platform-specific compiler and linker flags
 #
-IF ( NOT OS_WIN )
+IF ( NOT GBX_OS_WIN )
     ADD_DEFINITIONS( "-Wall" )
-ELSE ( NOT OS_WIN )
+ELSE ( NOT GBX_OS_WIN )
     ADD_DEFINITIONS( "-Wall -D_CRT_SECURE_NO_DEPRECATE" )
-ENDIF ( NOT OS_WIN )
+ENDIF ( NOT GBX_OS_WIN )
