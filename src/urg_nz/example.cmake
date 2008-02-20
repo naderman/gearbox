cf37e06ea418f61d23b3aeb4fa88015be0c85004
borax00 <borax00@4b7f92db-2845-0410-95f1-b1f669fef64b>
2008-02-20 00:32:37 +0000

    rolling back accidental commit
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@73 4b7f92db-2845-0410-95f1-b1f669fef64b

--- src/urg_nz/example.cmake
+++ src/urg_nz/example.cmake
@@ -1,10 +1,10 @@
 PROJECT( urg_nz_example )
 
-INCLUDE_DIRECTORIES( /usr/local )
+INCLUDE_DIRECTORIES( @CMAKE_INSTALL_PREFIX@ )
 
 ADD_EXECUTABLE( urg_nz_example example.cpp )
 TARGET_LINK_LIBRARIES( urg_nz_example urg_nz )
 SET_TARGET_PROPERTIES( urg_nz_example PROPERTIES
-                       LINK_FLAGS "-L/usr/local/lib/gearbox"
-                       INSTALL_RPATH "${INSTALL_RPATH};/usr/local/lib/gearbox"
+                       LINK_FLAGS "-L@CMAKE_INSTALL_PREFIX@/lib/gearbox"
+                       INSTALL_RPATH "${INSTALL_RPATH};@CMAKE_INSTALL_PREFIX@/lib/gearbox"
                        BUILD_WITH_INSTALL_RPATH TRUE )
