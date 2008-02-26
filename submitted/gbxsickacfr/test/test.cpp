539e315dd3a9270cd8e1183d91e06ed526d91109
borax00 <borax00@4b7f92db-2845-0410-95f1-b1f669fef64b>
2008-02-26 01:49:30 +0000

    formatting.
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@88 4b7f92db-2845-0410-95f1-b1f669fef64b

--- submitted/gbxsickacfr/test/test.cpp
+++ submitted/gbxsickacfr/test/test.cpp
@@ -33,7 +33,7 @@ int main( int argc, char **argv )
     string port = "/dev/ttyS0";
 
     // Get some options from the command line
-    while ((opt = getopt (argc, argv, "p:b:")) != -1)
+    while ((opt = getopt(argc, argv, "p:b:")) != -1)
     {
         switch ( opt )
         {
