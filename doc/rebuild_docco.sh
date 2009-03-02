ab5b48c8620dfca0d3f9105544854b346a49dcc2
russo2503v <russo2503v@4b7f92db-2845-0410-95f1-b1f669fef64b>
2009-03-02 08:11:52 +0000

    adapted to new SF access procedures
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@386 4b7f92db-2845-0410-95f1-b1f669fef64b

--- doc/rebuild_docco.sh
+++ doc/rebuild_docco.sh
@@ -14,15 +14,33 @@ force () {
     fi
 }
 
-# doxygen
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <user-name>"
+  exit 1
+fi
+
 DOXYGENCMD=/usr/bin/doxygen
 DOXYFILE=doxyfile
 TARBALL=gearbox-release-doc.tar.gz
+SFUSER=$1
+SFSCPHOST=web.sf.net
+SFSHELLHOST=shell.sf.net
+SFPROJECT=gearbox
+SFDIR=/home/groups/g/ge/gearbox/htdocs/gearbox
 
 force ls
 force $DOXYGENCMD $DOXYFILE
+
+# direct file copy, works but is very slow
+#scp -r html/* $SFUSER@$SFSCPHOST:$SFDIR/.
+
+# with tarball
 force cd html
 force tar --exclude=$TARBALL -zcvf $TARBALL *
-force scp $TARBALL shell.sourceforge.net:
+force scp $TARBALL $SFUSER@$SFSCPHOST:
 
-ssh shell.sourceforge.net "cd /home/groups/g/ge/gearbox/htdocs/; mv ~/$TARBALL .; tar -zxvf $TARBALL"
+# don't know how to combine this login with commands, do it manually
+echo "when connected execute this:"
+echo "cd $SFDIR; mv ~/$TARBALL .; tar -zxvf $TARBALL"
+echo "to quit, type 'shutdown'"
+force ssh -t $SFUSER,$SFPROJECT@$SFSHELLHOST create #"cd $SFDIR; mv ~/$TARBALL .; tar -zxvf $TARBALL"
