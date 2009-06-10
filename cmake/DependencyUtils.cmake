8f6aadb097568f5f1c8000c9fdc5c88ae8224110
russo2503v <russo2503v@4b7f92db-2845-0410-95f1-b1f669fef64b>
2009-06-10 06:45:09 +0000

    removed obsolete commented out stuff
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@420 4b7f92db-2845-0410-95f1-b1f669fef64b

--- cmake/DependencyUtils.cmake
+++ cmake/DependencyUtils.cmake
@@ -37,21 +37,6 @@ macro( GBX_UTIL_MAKE_OPTION_NAME option_name module_type module_name )
 endmacro( GBX_UTIL_MAKE_OPTION_NAME option_name module_name )
 
 #
-# This is a utility macro for internal use.
-# (there's another copy of this simple macro in TargetUtils.cmake)
-#
-# macro( GBX_UTIL_MAKE_MANIFEST_NAME manifest_name module_name )
-# #     message( STATUS "GBX_UTIL_MAKE_MANIFEST_NAME [ manifest_name=${manifest_name}, MOD_NAME=${module_name} ]" )
-# 
-#     string( TOUPPER ${module_name} module_name_upper )
-#     # dereference the variable name once, so that we are setting the variable in the calling context!
-#     set( ${manifest_name} ${module_name_upper}_INSTALLED )
-# 
-# #     message( STATUS "GBX_UTIL_MAKE_MANIFEST_NAME output: manifest_name=${${manifest_name}}" )
-# endmacro( GBX_UTIL_MAKE_MANIFEST_NAME manifest_name module_name )
-
-
-#
 # GBX_REQUIRE_OPTION( cumulative_var [EXE | LIB] module_name default_option_value [option_name] [OPTION DESCRIPTION] )
 #
 # E.g.
@@ -137,55 +122,6 @@ macro( GBX_REQUIRE_VAR cumulative_var module_type module_name test_var reason )
 
 endmacro( GBX_REQUIRE_VAR cumulative_var module_type module_name test_var reason )
 
-# OBSOLETE! CONVERTED FROM HOME-MADE MANIFESTS TO FIND_PACKAGE()
-#
-# GBX_REQUIRE_install( cumulative_var [EXE | LIB] module_name installed_module [reason] )
-#
-# A special case of REQUIRE_VAR. Checks whether a manifest variable is defined 
-# for the module with a name "installed_module".
-# E.g.
-# Initialize a variable first
-#   set( build TRUE )
-# Now test the variable value
-#   REQUIRE_install( build LIB HydroStuff GbxStuff )
-#           will check if GBXSTUFF_INSTALLED is defined.
-#   This example is equivalent to
-#   REQUIRE_VAR( build LIB HydroStuff GBXSTUFF_INSTALLED "GbxStuff was not installed" )
-#
-# macro( GBX_REQUIRE_INSTALL cumulative_var module_type module_name installed_module )
-# 
-#     # debug
-# #     message( STATUS "GBX_REQUIRE_INSTALL [ CUM_VAR=${cumulative_var}, MOD_TYPE=${module_type}, MOD_NAME=${module_name}, installed_module=${installed_module} ]" )
-# 
-#     GBX_UTIL_MAKE_MANIFEST_NAME( test_var ${installed_module} )
-# 
-#     if( ${ARGC} GREATER 5 )
-#         set( reason ${ARGV6} )
-#     else( ${ARGC} GREATER 5 )
-#         set( reason "${installed_module} was not installed" )
-#     endif( ${ARGC} GREATER 5 )
-# 
-#     # must dereference both var names once (!)
-#     GBX_REQUIRE_VAR( ${cumulative_var} ${module_type} ${module_name} ${test_var} ${reason} )
-# 
-# endmacro( GBX_REQUIRE_INSTALL cumulative_var module_type module_name installed_module )
-
-# OBSOLETE! CONVERTED FROM HOME-MADE MANIFESTS TO FIND_PACKAGE()
-#
-# GBX_REQUIRE_INSTALLS( cumulative_var [EXE | LIB] module_name target0 [targe1 target2 ...] )
-#
-# macro( GBX_REQUIRE_INSTALLS cumulative_var module_type module_name )
-#     
-#     if( ${ARGC} LESS 4 )
-#         message( FATAL_ERROR "GBX_REQUIRE_INSTALLS macro needs to at least one target name (${ARGC} params were given)." ) 
-#     endif( ${ARGC} LESS 4 )
-# 
-#     foreach( trgt ${ARGN} )
-#         GBX_REQUIRE_install( ${cumulative_var} ${module_type} ${module_name} ${trgt} )
-#     endforeach( trgt ${ARGN} )
-# 
-# endmacro( GBX_REQUIRE_INSTALLS cumulative_var module_type module_name )
-
 #
 # GBX_REQUIRE_LIB( cumulative_var [EXE | LIB] module_name target_name [reason] )
 #
@@ -217,13 +153,10 @@ macro( GBX_REQUIRE_LIB cumulative_var module_type module_name target_name )
         if( NOT target_location  )
             set( ${cumulative_var} FALSE )
             GBX_UTIL_MAKE_OPTION_NAME( option_name ${module_type} ${module_name} )
-            # alexm: disabling dependency caching
             if( is_exe )
                 GBX_NOT_ADD_EXECUTABLE( ${module_name} ${reason} )
-#                set( ${option_name} OFF CACHE BOOL "Try to build ${module_name}" FORCE )
             else( is_exe )
                 GBX_NOT_add_library( ${module_name} ${reason} )
-#                set( ${option_name} OFF CACHE BOOL "Try to build lib${module_name} library" FORCE )
             endif( is_exe )
         endif( NOT target_location )
 
@@ -249,74 +182,6 @@ macro( GBX_REQUIRE_LIBS cumulative_var module_type module_name )
     endforeach( trgt ${ARGN} )
 
 endmacro( GBX_REQUIRE_LIBS cumulative_var module_type module_name )
-
-# OBSOLETE! Use GBX_REQUIRE_LIB instead.
-#
-# GBX_REQUIRE_LIB( cumulative_var [EXE | LIB] module_name depend_name [reason] )
-#
-# This is a convenience macro which combines the functionality of GBX_REQUIRE_INSTALL and GBX_REQUIRE_LIB.
-# It can be used to require executables despite the name.
-#
-# First checks if a target called ${depend_name} exists, i.e. a library or executable with this name
-# is part of the same project and is enabled. If it exists, sets ${cumulative_var} to TRUE and exits.
-#
-# Otherwise, checks if a library or executable called ${depend_name} is installed, i.e. a manifest 
-# variable is defined.
-#
-# E.g.
-# Initialize a variable first
-#   set( build TRUE )
-# Now set up and test option value
-#   GBX_REQUIRE_LIB ( build EXE localiser HydroStuff )
-#   GBX_REQUIRE_LIB ( build LIB MyStuff HydroStuff )
-#
-# macro( GBX_REQUIRE_LIB cumulative_var module_type module_name depend_name )
-# #     message( STATUS "GBX_REQUIRE_LIB [ CUM_VAR=${${cumulative_var}}, MOD_TYPE=${module_type}, MOD_NAME=${module_name}, depend_name=${depend_name} ]" )
-# 
-#     if( ${cumulative_var}  )
-# 
-#         GBX_UTIL_CHECK_MODULE_TYPE( ${module_type} is_exe is_lib )
-#     
-#         if( ${ARGC} GREATER 5 )
-#             set( reason ${ARGV6} )
-#         else( ${ARGC} GREATER 5 )
-#             set( reason "${depend_name} is not being built and was not installed" )
-#         endif( ${ARGC} GREATER 5 )
-#     
-#         # first, look for a target
-#         GET_TARGET_PROPERTY( target_location ${depend_name} LOCATION )
-#     
-#         # must dereference both var and option names once (!) and IF will evaluate their values
-#         if( NOT target_location  )
-#             
-#             # target not found, look for an install
-#             GBX_UTIL_MAKE_MANIFEST_NAME( test_var ${depend_name} )
-# 
-#             # must dereference both var names once (!)
-#             GBX_REQUIRE_VAR( ${cumulative_var} ${module_type} ${module_name} ${test_var} ${reason} )
-# 
-#         endif( NOT target_location )
-# 
-#     endif( ${cumulative_var}  )
-# 
-# endmacro( GBX_REQUIRE_LIB cumulative_var module_type module_name target_name )
-
-# OBSOLETE! Use GBX_REQUIRE_LIBS instead.
-#
-# GBX_REQUIRE_LIBS( cumulative_var [EXE | LIB] module_name LIB0 [LIB1 LIB2 ...] )
-#
-# macro( GBX_REQUIRE_LIBS cumulative_var module_type module_name )
-# #     message( STATUS "GBX_REQUIRE_LIBS [ CUM_VAR=${${cumulative_var}}, MOD_TYPE=${module_type}, MOD_NAME=${module_name}, target_names=${ARGN} ]" )
-# 
-#     if( ${ARGC} LESS 4 )
-#         message( FATAL_ERROR "GBX_REQUIRE_LIBS macro needs to at least one library name (${ARGC} params were given)." )
-#     endif( ${ARGC} LESS 4 )
-# 
-#     foreach( lib ${ARGN} )
-#         GBX_REQUIRE_LIB( ${cumulative_var} ${module_type} ${module_name} ${lib} )
-#     endforeach( lib ${ARGN} )
-# 
-# endmacro( GBX_REQUIRE_LIBS cumulative_var module_type module_name )
 
 #
 # This is a utility macro for internal use.
