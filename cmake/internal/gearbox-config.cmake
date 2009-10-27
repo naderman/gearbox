6f6dbf9b5d22049790e71156194e9d6b2ee07a04
russo2503v <russo2503v@4b7f92db-2845-0410-95f1-b1f669fef64b>
2009-10-27 03:02:57 +0000

    gearbox-config.cmake now reflects the actual install dir
    git-svn-id: https://gearbox.svn.sf.net/svnroot/gearbox/gearbox/trunk@447 4b7f92db-2845-0410-95f1-b1f669fef64b

--- cmake/internal/gearbox-config.cmake
+++ cmake/internal/gearbox-config.cmake
@@ -11,24 +11,24 @@ get_filename_component( _found_dir "${CMAKE_CURRENT_LIST_FILE}" PATH )
 include( ${_found_dir}/gearbox-targets.cmake )
 
 # assume that gearbox-config.cmake was installed into
-# <install-root>/lib/gearbox/ 
+# <install-root>/lib/gearbox/
 set( _install_dir "${_found_dir}/../../" )
 
-get_filename_component( 
-    GEARBOX_INCLUDE_DIR 
-    "${_install_dir}/include/gearbox" 
+get_filename_component(
+    GEARBOX_INCLUDE_DIR
+    "${_install_dir}/include/gearbox"
     ABSOLUTE )
 
-get_filename_component( 
-    GEARBOX_CMAKE_DIR 
-    "${_install_dir}/share/gearbox/cmake"
+get_filename_component(
+    GEARBOX_CMAKE_DIR
+    "${_install_dir}/share/cmake/Modules"
     ABSOLUTE )
 
 set( GEARBOX_USE_FILE "${GEARBOX_CMAKE_DIR}/gearbox-use-file.cmake" )
 
-get_filename_component( 
-    GEARBOX_LINK_DIR 
-    "${_install_dir}/lib/gearbox" 
+get_filename_component(
+    GEARBOX_LINK_DIR
+    "${_install_dir}/lib/gearbox"
     ABSOLUTE )
 
 set( _found_dir )
